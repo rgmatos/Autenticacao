3cd9878ab7ed50ea37e66f4650835e845b745491
rgmatos <54383685+rgmatos@users.noreply.github.com>
2022-01-26 17:03:56 -0300

    Create index.tsx

---
+++ src/components/Menus/Form/Auth/SignIn/index.tsx
@@ -0,0 +1,33 @@
+import { findByLabelText } from '@testing-library/react';
+import React, {useState, useEffect} from 'react'
+import styled from 'styled-components';
+import { collapseTextChangeRangesAcrossMultipleVersions } from 'typescript';
+
+
+export default function SignIn() {
+
+    const Container = Styled.div
+        width: 100%;
+        heught: calc(100vh - 200px;
+        form{ 
+            width: 50%;
+            display: flex;
+            flex-direction: collumn;
+        }
+   
+   
+   
+    return (
+        <Container>
+
+
+
+        </Container>
+
+
+
+
+
+
+    )
+}
